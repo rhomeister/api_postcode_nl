d68ad428502d5fb7f60b2114d27b18851b9588c0
Matijs van Zuijlen <matijs@matijs.net>
2015-09-18 10:56:40 +0200

    Make tests not access the live API

--- test/test_api_postcode_nl.rb
+++ test/test_api_postcode_nl.rb
@@ -1,8 +1,16 @@
 require 'helper'
 
 class TestApiPostcodeNl < Test::Unit::TestCase
+  class DummyFetcher
+    def fetch(*_)
+      OpenStruct.new(body: '{}')
+    end
+  end
+
   context 'API.address' do
     setup do
+      ApiPostcodeNl::API.fetcher = DummyFetcher.new
+
       ApiPostcodeNl::API.key = 'some-api-key'
       ApiPostcodeNl::API.secret = 'super-secret'
     end
